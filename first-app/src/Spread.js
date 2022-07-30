7874820d6496fe1ee90041b4c782eb745c8ce276
Mayank <explorermayank94@gmail.com>
2022-07-30 23:58:51 +0530

    spread2

--- first-app/src/Spread.js
+++ first-app/src/Spread.js
@@ -1,5 +1,9 @@
 let arr=[1,2,3];
-let arr2=arr;
-arr2.push(4);
-console.log(arr);
-console.log(arr2);
+// let arr2=arr;
+// arr2.push(4);
+// console.log(arr);
+// console.log(arr2);
+
+console.log(arr);// prints the array
+//using spread operator (...) is k/a spread operator 
+console.log(...arr); // prints the lists after spreading 

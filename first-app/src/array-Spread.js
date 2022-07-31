f71b5a4480a21a53b672adab76fb72a586148878
Mayank <explorermayank94@gmail.com>
2022-07-31 09:28:58 +0530

    operator spread.js

--- first-app/src/array-Spread.js
+++ first-app/src/array-Spread.js
@@ -9,6 +9,8 @@ let arr=[1,2,3];
 //console.log(...arr); // prints the lists after spreading 
 
 let arr2=[...arr]; //store the list in new array and manipulate it.
+//so that next variable gets new address
+//we basically spread the old values 
 arr2.push(4);
 console.log(arr);
 console.log(arr2);
